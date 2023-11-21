d21969c5c421662a58fba60b7045dfa8fbd93254
Ricardo <ricardo@yonkolevel.com>
2023-11-21 15:12:47 +0000

    add release and debug config options

--- Package.swift
+++ Package.swift
@@ -1,19 +1,14 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "SwiftyTracktionEngine",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftyTracktionEngine",
-            targets: ["SwiftyTracktionEngine", "ObjCTacktionEngine",]),
+            targets: ["SwiftyTracktionEngine", "ObjCTacktionEngine"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftyTracktionEngine",
             dependencies: ["ObjCTacktionEngine"],
@@ -23,11 +18,15 @@ let package = Package(
             name: "ObjCTacktionEngine",
             path: "Sources/ObjC",
             cxxSettings: [
-//                .define("JUCE_APP_CONFIG_HEADER", to: "\"TracktionEngineWrapper.h\""),
-                .define("JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED", to: "1"),
+                               .define("JUCE_APP_CONFIG_HEADER", to: "\"TracktionEngineWrapper.h\""),
+
                 .headerSearchPath("../CPP/modules/juce/modules"),
                 .headerSearchPath("../CPP/modules"),
                 .unsafeFlags(["-std=c++17"]),
+
+                // Conditional compilation settings for Debug and Release
+                .define("DEBUG", .when(configuration: .debug)),
+                .define("NDEBUG", .when(configuration: .release)),
             ]
         ),
     ]
